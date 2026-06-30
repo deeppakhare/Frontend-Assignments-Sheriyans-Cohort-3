278522f2f3269ec65c070079d5a99b66ca4237b4
justforit4102 <deeppakhare19@gmail.com>
2026-06-30 23:02:48 +0530

    navbar

--- Assignment-8/app.js
+++ Assignment-8/app.js
@@ -1,12 +1,16 @@
 const isLoggedIn = localStorage.getItem("isLoggedIn");
 const userName = document.querySelectorAll('.userName'); 
 const currentUser = localStorage.getItem('currentUser');
-
+const logoutBtn = document.getElementById('logoutBtn');
 
 if (isLoggedIn !== "true") {
     window.location.href = "login.html";
 }
 
+logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = "login.html";
+})
 
 
 if (currentUser) {
