bc2b932f5fb1864f53609f1d760f41e073a847d6
justforit4102 <deeppakhare19@gmail.com>
2026-06-30 12:14:58 +0530

    grid layout

--- Assignment-8/app.js
+++ Assignment-8/app.js
@@ -1,5 +1,18 @@
 const isLoggedIn = localStorage.getItem("isLoggedIn");
+const userName = document.querySelectorAll('.userName'); 
+const currentUser = localStorage.getItem('currentUser');
+
 
 if (isLoggedIn !== "true") {
     window.location.href = "login.html";
 }
+
+
+
+if (currentUser) {
+  userName.forEach(span => {
+    span.textContent = currentUser;
+  });
+}
+
+
